0c4af42e1c121453bae067fbf4e3e44d78b50438
pika <michael>
2021-04-23 11:04:24 +0800

    [update] clean up initial build script

--- initial_build.sh
+++ initial_build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-cd /SOURCE
-git clone https://github.com/Euclideon/udsdksamples
-cd udsdksamples/external
-git clone https://github.com/Euclideon/udcore
+# -----------
+# udSDK 2.1 from official download
+# -----------
+cp -r /data/udSDK /SOURCE/udsdk # from udSDK_Developer_2.1.zip
 cp /SOURCE/udsdk/lib/ubuntu18.04_GCC_x64/libudSDK.so /usr/local/lib
+
+# -----------
+# udstream sample program from github
+# -----------
+cd /SOURCE
+git clone --depth 1 https://github.com/Euclideon/udsdksamples
+cd udsdksamples/external
+git clone --depth 1 https://github.com/Euclideon/udcore
+
+# -----------
+# build script and demo build
+# -----------
 cd /SOURCE/udsdksamples/languages/cpp
 cp /data/CMakeLists.txt ./
-cp -r /data/udSDK /SOURCE/udsdk
 mkdir build
+mkdir bin
 cd build
 cmake ..
-make
+make -j1
